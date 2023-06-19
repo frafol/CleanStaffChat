cc1b92b60f1eb405506ee16c452d1694c0abd8f7
Francesco <67754059+frafol@users.noreply.github.com>
2023-06-19 09:14:49 +0200

    Fix my autism

--- src/main/java/it/frafol/cleanss/bungee/objects/Placeholder.java
+++
@@ -1,9 +0,0 @@
-package it.frafol.cleanss.bungee.objects;
-
-import lombok.Getter;
-
-@Getter
-public class Placeholder { private final String key;private final String value;
-    public Placeholder(String key, String value) {this.key = "%" + key + "%";this.value = value;
-    }
-}
--- src/main/java/it/frafol/cleanss/velocity/objects/Placeholder.java
+++
@@ -1,9 +0,0 @@
-package it.frafol.cleanss.velocity.objects;
-
-import lombok.Getter;
-
-@Getter
-public class Placeholder { private final String key;private final String value;
-    public Placeholder(String key, String value) {this.key = "%" + key + "%";this.value = value;
-    }
-}
